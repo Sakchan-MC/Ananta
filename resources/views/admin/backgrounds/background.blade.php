3a810f90c5afbd85f281f31fca2da971592b5e96
Sakchan-MC <sakchan.mc@gmail.com>
2021-12-05 16:31:46 +0700

    fix background

--- resources/views/admin/backgrounds/background.blade.php
+++ resources/views/admin/backgrounds/background.blade.php
@@ -14,11 +14,7 @@
             <table class="table">
                 <thead>
                     <tr>
-<<<<<<< HEAD
                         <th scope="col" class="col-md-2">รูปภาพ</th>
-=======
-                        <th scope="col" class="col-md-2"></th>
->>>>>>> f751e38cdcd464d5e4e0a666b68f77092f0656e2
                         <th scope="col" class="col-md-2">สถานะ</th>
                         <th scope="col" class="col-md-2">การกระทำ</th>
                     </tr>
@@ -32,7 +28,6 @@
                                 class="background-img" />
                         </td>
                         <td class="col-2">
-<<<<<<< HEAD
                             @if($background->status==0)
                             <a href="#" class="btn btn-success"
                                 onclick="changestatus0('{{$background->id}}','background') ">เปิด</a>
@@ -48,15 +43,6 @@
                             <a href="#" class="btn btn-success"
                                 onclick="editimg('{{$background->id}}','background')">แก้ไข</a>
                             <a href="#" onclick="deleteConfirm('{{$background->id}}','background') "
-=======
-                            @if($background->status==0) ปิด
-                            @else เปิด
-                            @endif</td>
-                        <td class="col-3">
-                            <a href="{{url('/admin/backgrounds/edit/'.$background->id)}}"
-                                class="btn btn-success">แก้ไข</a>
-                            <a href="{{url('/admin/backgrounds/delete/'.$background->id)}}"
->>>>>>> f751e38cdcd464d5e4e0a666b68f77092f0656e2
                                 class="btn btn-danger">ลบ</a>
                         </td>
 
